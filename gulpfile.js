fbcb6507d0c059b881eb45f95c5f20ca95df8a71
Andy Carter <andy1704@gmail.com>
2015-06-09 22:16:43 +0100

    Throw exception when jsonlint fails so that gulp no longer exits 0.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 // Include gulp and plugins
 var gulp = require('gulp');
-var jsonlint = require("gulp-jsonlint");
+var jsonlint = require('gulp-jsonlint');
+var gutil = require('gulp-util');
+var exit = require('gulp-exit');
 
 gulp.task('lint', function () {
-	return gulp.src('./src/*.json')
-		.pipe(jsonlint())
-		.pipe(jsonlint.reporter());
+  return gulp.src('./src/*.json')
+    .pipe(jsonlint())
+    .pipe(jsonlint.reporter(failReporter))
+    .pipe(exit());
 });
+
+var failReporter = function (file) {
+  if (file.jsonlint && !file.jsonlint.success) {
+    throw new gutil.PluginError('gulp-jsonlint', 'jsonlint failed for ' + file.relative);
+  }
+};
 
 gulp.task('ci', ['lint']);
