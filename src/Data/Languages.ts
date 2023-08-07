e6670bd5d8eade6e40de9c65a130fd7154dacfd5
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-08-08 02:36:51 +0700

    feat(types): Languages

--- src/Data/Languages.ts
+++ src/Data/Languages.ts
@@ -1,1 +1,7 @@
-export interface ILanguages {}
+export interface ILanguages {
+	officialLanguages: string[]
+	regionalLanguage: string[]
+	minorityLanguage: string[]
+	nationalLanguage: string[]
+	widelySpoken: string[]
+}
