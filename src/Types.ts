b678954cf292cf51b2720ac0614ae782623b0d69
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-08-08 00:43:15 +0700

    feat(types): Empty interface

--- src/Types.ts
+++ src/Types.ts
@@ -8,4 +8,62 @@ interface IISO3166Data {
 	numeric: string
 }
 
+interface IAverageHeightData {}
+
+interface ICountriesData {}
+
+interface ICountriesNameData {}
+
+interface IGS1CodeData {}
+
+interface ICallingCodesData {}
+
+interface ICapitalCityData {}
+
+interface ICitiesData {}
+
+interface IContinentData {}
+
+interface ICoastlineData {}
+
+interface IAreaData {}
+
+interface IISO4217Data {}
+
+interface ICurrencySymbolData {}
+
+interface ICurrencyNameData {}
+
+interface IccTLDData {}
+
+interface IAverageElevationData {}
+
+interface IFlagData {}
+
+interface IGeoCoordinateData {}
+
+interface IIndependenceData {}
+
+interface ILandlockedData {}
+
+interface ILanguagesData {}
+
+interface ILifeExpectancyData {}
+
+interface INationalDishesData {}
+
+interface INationalSymbolData {}
+
+interface IPopulationDensityData {}
+
+interface IPopulationData {}
+
+interface IRegionData {}
+
+interface IReligionData {}
+
+interface IYearlyAverageTemperatureData {}
+
+interface INationalSportData {}
+
 type IData<T extends Record<string, any>> = (IBaseData & T)[]
