26597173f909d9738e4223e1dabc404f02d8ccc9
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-08-08 01:53:22 +0700

    feat(types): GS1 code

--- src/Data/GS1Code.ts
+++ src/Data/GS1Code.ts
@@ -1,1 +1,20 @@
-export interface IGS1Code {}
+interface IGS1CodeRange {
+	isRange: true
+
+	/**
+	 * Inclusive
+	 */
+	start: number
+
+	/**
+	 * Inclusive
+	 */
+	end: number
+}
+
+interface IGS1CodeSingle {
+	isRange: false
+	number: number
+}
+
+export type IGS1Code = IGS1CodeSingle | IGS1CodeRange
