8a88d48d24c6acd674f237e9f567804bb8b75349
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-08-09 12:55:39 +0700

    feat(data): Scrap united nations member

--- src/Data/UnitedNationsMember.ts
+++ src/Data/UnitedNationsMember.ts
@@ -1,8 +1,64 @@
+import { parse } from 'node-html-parser'
+import MonthNameToIndex from '../Utils/MonthNameToIndex.js'
+
 export interface IUnitedNationsMember {
 	isOriginalMember: boolean
 
-    /**
-     * Expoch unix timestamp
-     */
+	/**
+	 * Expoch unix timestamp
+	 */
 	dateOfAdmission: number
 }
+
+const ScrapUnitedNationsMember = async () => {
+	const rawHtml = await fetch(
+		'https://en.wikipedia.org/wiki/Member_states_of_the_United_Nations'
+	).then(response => response.text())
+
+	const root = parse(rawHtml)
+
+	const dataTableBody = root.querySelector(
+		'.wikitable.sortable.plainrowheaders tbody'
+	)
+
+	// Select all rows except header row element
+	const rows = dataTableBody.querySelectorAll('tr:not(:has(th[scope=col]))')
+
+	for (const row of rows) {
+		// Get all direct children
+		const children = row.querySelectorAll('> *')
+
+		const country = children[0].querySelector('a').innerText
+		const dateOfAdmissionString =
+			children[1].querySelector('span').innerText
+		const isOriginalMember =
+			children[2].getAttribute('data-sort-value') === 'Yes'
+
+		const [
+			dateOfAdmissionDateString,
+			dateOfAdmissionMonthNameString,
+			dateOfAdmissionYearString,
+		] = dateOfAdmissionString.split(' ')
+
+		const dateOfAdmissionMilliseconds = Date.UTC(
+			parseInt(dateOfAdmissionYearString),
+			MonthNameToIndex(dateOfAdmissionMonthNameString) as number, // Assume wikipedia date is always valid
+			parseInt(dateOfAdmissionDateString, 10),
+			0,
+			0,
+			0,
+			0
+		)
+
+        // Unix epoch
+		const dateOfAdmission = dateOfAdmissionMilliseconds / 1000
+
+		console.log({
+			country,
+			dateOfAdmission,
+			isOriginalMember,
+		})
+	}
+}
+
+export default ScrapUnitedNationsMember
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import ScrapISO3166 from "./Data/ISO3166.js"
+import ScrapISO3166 from './Data/ISO3166.js'
+import ScrapUnitedNationsMember from './Data/UnitedNationsMember.js'
 
 console.log('Country JSON Scrapper!')
 
-await ScrapISO3166()
+// await ScrapISO3166()
+await ScrapUnitedNationsMember()
