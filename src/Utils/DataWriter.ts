6fe194ed32518f101f9b34ff62ef811941f49d47
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-08-14 17:18:34 +0700

    feat: Write data

--- src/Utils/DataWriter.ts
+++ src/Utils/DataWriter.ts
@@ -1,16 +1,14 @@
-import { fileURLToPath } from 'url'
 import path from 'path'
 import fs from 'fs/promises'
 
 import { IData } from '../Types'
 
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
-
-const outputDir = '/dist/'
-
-const DataWriter = async <T>(data: IData<T>, fileName: string) => {
-	const filePath = path.join(dirname, outputDir, `${fileName}.json`)
+const DataWriter = async <T>(
+	outputPath: string,
+	data: IData<T>,
+	fileName: string
+) => {
+	const filePath = path.join(outputPath, `${fileName}.json`)
 
 	await fs.writeFile(filePath, JSON.stringify(data))
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,33 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+import fs from 'fs/promises'
+
 import ScrapAverageHeight from './Data/AverageHeight.js'
 import ScrapISO3166 from './Data/ISO3166.js'
 import ScrapUnitedNationsMember from './Data/UnitedNationsMember.js'
 import CountriesNameFromData from './Utils/CountriesNameFromData.js'
+import DataWriter from './Utils/DataWriter.js'
 import NormalizeData from './Utils/NormalizeData.js'
 import Wait from './Utils/Wait.js'
 
 const delayBetweenRequest = 2000
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+const outputDir = '/dist/'
+
+const outputPath = path.join(dirname, '../', outputDir)
+
+// Ensure output dir exist
+// Remove directory if exist
+try {
+	await fs.access(outputPath, fs.constants.F_OK)
+	await fs.rm(outputPath, { recursive: true })
+} catch {}
+
+// Re create
+await fs.mkdir(outputPath)
 
 console.log('Country JSON Scrapper!')
 
@@ -16,9 +38,13 @@ await Wait(delayBetweenRequest)
 
 const ISO3166 = NormalizeData(await ScrapISO3166(), validCountriesName)
 
-await Wait(delayBetweenRequest)
+await DataWriter(outputPath, ISO3166, 'ISO3166')
 
-const averageHeight = NormalizeData(
-	await ScrapAverageHeight(),
-	validCountriesName
-)
+// await Wait(delayBetweenRequest)
+
+// const averageHeight = NormalizeData(
+// 	await ScrapAverageHeight(),
+// 	validCountriesName
+// )
+
+// await DataWriter(outputPath, averageHeight, 'averageHeight')
