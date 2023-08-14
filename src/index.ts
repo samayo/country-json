65c43d0936e34713a66dd85d03ff0d500088c0ec
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-08-14 15:41:24 +0700

    feat: Delay between request

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,23 @@ import ScrapAverageHeight from './Data/AverageHeight.js'
 import ScrapISO3166 from './Data/ISO3166.js'
 import ScrapUnitedNationsMember from './Data/UnitedNationsMember.js'
 import CountriesNameFromData from './Utils/CountriesNameFromData.js'
+import NormalizeData from './Utils/NormalizeData.js'
+import Wait from './Utils/Wait.js'
+
+const delayBetweenRequest = 2000
 
 console.log('Country JSON Scrapper!')
 
 const unitedNationsMembers = await ScrapUnitedNationsMember()
 const validCountriesName = CountriesNameFromData(unitedNationsMembers)
 
-// await ScrapISO3166()
-await ScrapAverageHeight()
+await Wait(delayBetweenRequest)
+
+const ISO3166 = NormalizeData(await ScrapISO3166(), validCountriesName)
+
+await Wait(delayBetweenRequest)
+
+const averageHeight = NormalizeData(
+	await ScrapAverageHeight(),
+	validCountriesName
+)
