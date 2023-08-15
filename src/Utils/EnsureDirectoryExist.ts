dba7a1ad0fa6d33673d687fafa481669878c4d82
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-08-15 17:32:37 +0700

    feat(utils): Ensure directory exist and empty

--- src/Utils/EnsureDirectoryExist.ts
+++ src/Utils/EnsureDirectoryExist.ts
@@ -3,8 +3,12 @@ import fs from 'fs/promises'
 const EnsureDirectoryExist = async (path: string) => {
 	try {
 		await fs.access(path, fs.constants.F_OK)
+
+		return true
 	} catch {
 		await fs.mkdir(path)
+
+		return false
 	}
 }
 
